7609659667fec767bde28d72dbe5b046653e61d1
Margdarshak <margdarshaklibrary@example.com>
2026-07-05 16:10:48 +0530

    fix bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://margdarshak.in',
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   integrations: [
     sitemap({
